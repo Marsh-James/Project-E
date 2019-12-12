8302520572208bd347c76a21924944e076e2da7b
James <jamesfkmarsh@gmail.com>
2019-12-12 12:14:51 +0000

    added predictor and dir reshuffling

--- cnn/model/src.py
+++ cnn/model/src.py
@@ -1,9 +1,13 @@
+from os import path
+
+from keras.engine.saving import load_model
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten, Lambda, MaxPooling2D, Dropout
 from sklearn.model_selection import train_test_split
 from keras.preprocessing import image
 from keras.backend import resize_images
 from keras_preprocessing.image import ImageDataGenerator
+import pandas as pd
 
 import os
 from PIL import Image
@@ -56,37 +60,72 @@ validation_data_gen = train_datagenerator.flow_from_directory(
     batch_size=32,
     subset="validation")
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(128, 128, 1)))
-model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(128, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(256, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.2))
-model.add(Conv2D(512, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.3))
-model.add(Conv2D(768, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.4))
-model.add(Flatten())
-model.add(Dense(15, activation='softmax'))
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+def predictions():
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-# model.fit(x_train, y_train,
-#           validation_data=(x_validation, y_validation),
-#           epochs=15)
+    test_generator = test_datagen.flow_from_directory(
+        "../data/testing/",
+        target_size=(128, 128),
+        color_mode="grayscale",
+        batch_size=32,
+        shuffle=False
+    )
 
-model.fit_generator(
-    train_data_gen,
-    steps_per_epoch=96,
-    validation_data=validation_data_gen,
-    validation_steps=32,
-    epochs=15,
-)
+    test_generator.reset()
 
-model.save('model-v1.h5')
+    model = load_model('model-v1.h5')
+
+    pred = model.predict_generator(test_generator, verbose=1)
+    predicted_class_indices = np.argmax(pred, axis=1)
+
+    labels = (train_data_gen.class_indices)
+    labels = dict((v, k) for k, v in labels.items())
+    predictions = [labels[k] for k in predicted_class_indices]
+
+    filenames = test_generator.filenames
+    results = pd.DataFrame({"Filename": filenames,
+                            "Predictions": predictions})
+    results.to_csv("./predictions.csv", index=None, header=False)
+    print(results)
+
+
+if not path.exists("model-v1.h5"):
+
+    model = Sequential()
+    model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(128, 128, 1)))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Conv2D(256, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(512, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(768, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(15, activation='softmax'))
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # model.fit(x_train, y_train,
+    #           validation_data=(x_validation, y_validation),
+    #           epochs=15)
+
+    model.fit_generator(
+        train_data_gen,
+        steps_per_epoch=96,
+        validation_data=validation_data_gen,
+        validation_steps=32,
+        epochs=15,
+    )
+
+    model.save('model-v1.h5')
+    predictions()
+else:
+    predictions()
