85382ec52a6c80f8879cf320daed42f129fc4c5b
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2019-12-13 04:43:45 +0000

    Completed Run 1 KNN algorithm. Added run1.txt results. Changed Java Version to Java 8

--- src/main/java/uk/ac/soton/ecs/compvisgroup/apple/run1/KNearest.java
+++ src/main/java/uk/ac/soton/ecs/compvisgroup/apple/run1/KNearest.java
@@ -1,8 +1,98 @@
 package uk.ac.soton.ecs.compvisgroup.apple.run1;
 
+import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.experiment.evaluation.classification.BasicClassificationResult;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.image.FImage;
+import org.openimaj.knn.DoubleNearestNeighboursExact;
+import org.openimaj.util.pair.IntDoublePair;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class KNearest {
-    public KNearest() {
+/**
+ * @author Oscar van Leusen
+ */
+class KNearest {
 
+    // DoubleNearestNeighboursExact requires feature vectors as double[][], not a List of DoubleFV.
+    private DoubleNearestNeighboursExact knn;
+    private int k;
+    private List<String> classes;
+
+    KNearest(int k) {
+        this.k = k;
+        }
+
+
+    void train(List<double[]> trainVectors, List<String> classes) {
+        this.knn = new DoubleNearestNeighboursExact(trainVectors.toArray(new double[][]{}));
+        this.classes = classes;
     }
+
+    public BasicClassificationResult<String> classify(TinyImageProcessor featureExtractor, FImage image) {
+        DoubleFV tinyImageVector = featureExtractor.extractFeature(image);
+
+        // Find neighbours in KNN
+        List<IntDoublePair> result = knn.searchKNN(tinyImageVector.values, this.k);
+
+
+        Map<String, Integer> neighbourClassCount = new HashMap<>();
+        // For each neighbour found, identify what class that neighbour is in and create a count.
+        for (IntDoublePair neighbour : result) {
+            // Of the IntDoublePair, the Int corresponds with the index of the trainVector and classes label list, so we
+            // can get the class label for that neighbour.
+            String imageClass = "null";
+            // If k-th nearest neighbour cannot be determined, returns index of -1. Without this condition I experienced
+            // IndexOutOfBoundsExceptions.
+            if (neighbour.first != -1) {
+                imageClass = classes.get(neighbour.first);
+            }
+
+            // Increment or create the count for that label.
+            if (neighbourClassCount.containsKey(imageClass)) {
+                int count = neighbourClassCount.get(imageClass) + 1;
+                neighbourClassCount.put(imageClass, count);
+            } else {
+                neighbourClassCount.put(imageClass, 1);
+            }
+        }
+
+        // Sort the neighbourClassCount descending to get most frequent neighbour classes.
+        LinkedHashMap<String, Integer> sortedClassCount = neighbourClassCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+
+        // Get the top prediction
+        String predictedClass = sortedClassCount.keySet().iterator().next();
+        int count = sortedClassCount.get(predictedClass);
+
+        // Calculate the confidence
+        double confidence = count / (double) this.k;
+        BasicClassificationResult<String> classificationResult = new BasicClassificationResult<>();
+        classificationResult.put(predictedClass, confidence);
+        return classificationResult;
+    }
+
+    public void validate(TinyImageProcessor featureExtractor, Set<Map.Entry<String, ListDataset<FImage>>> validationDataset) {
+        double correct = 0;
+        double incorrect = 0;
+
+        for (Map.Entry<String, ListDataset<FImage>> images : validationDataset) {
+            System.out.println("Evaluating key : " + images.getKey());
+            for (FImage image : images.getValue()) {
+                BasicClassificationResult<String> result = this.classify(featureExtractor, image);
+
+                // Increment counts according to whether prediction is the same as the label
+                if (result.getPredictedClasses().iterator().next().equals(images.getKey())) {
+                    correct += 1;
+                } else {
+                    incorrect += 1;
+                }
+            }
+        }
+
+        System.out.println("Accuracy: " + (correct / (correct + incorrect)));
+    }
+
 }
--- src/main/java/uk/ac/soton/ecs/compvisgroup/apple/run1/TinyImageProcessor.java
+++ src/main/java/uk/ac/soton/ecs/compvisgroup/apple/run1/TinyImageProcessor.java
@@ -1,38 +1,52 @@
 package uk.ac.soton.ecs.compvisgroup.apple.run1;
 
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processing.resize.ResizeProcessor;
-import org.openimaj.util.array.ArrayUtils;
-
-import java.util.Arrays;
 
 /**
  * @author Oscar van Leusen
  */
-public class TinyImageProcessor {
+class TinyImageProcessor implements FeatureExtractor<DoubleFV, FImage> {
 
     private int tinySize;
 
-    public TinyImageProcessor(int tinySize) {
+    TinyImageProcessor(int tinySize) {
         this.tinySize = tinySize;
     }
 
-    FImage generateTinyImage(FImage image) {
+    /**
+     * Generates a vector from the image using TinyImage ready for KNN.
+     * This will have zero mean and be normalised to unit length.
+     * @param image : Image to calculate TinyImage and then convert to vector
+     * @return
+     */
+    @Override
+    public DoubleFV extractFeature(FImage image) {
+        FImage tinyImage = generateTinyImage(image);
+        // Calculate the pixel mean, this can be subtracted from every pixel to zero-mean the Tiny Image
+        float pixelMean = tinyImage.sum() / (float) Math.pow(tinySize, 2);
+        FImage zeroMeanTinyImage = tinyImage.subtract(pixelMean);
+
+        // Flatten the image to a vector and then normalise it to unit length.
+        return new DoubleFV(zeroMeanTinyImage.getDoublePixelVector()).normaliseFV();
+    }
+
+    /**
+     * Take a regular FImage and make a tiny version of it, according to the tinySize provided in the Constructor
+     * @param image Regular Image
+     * @return Tiny Image
+     */
+    private FImage generateTinyImage(FImage image) {
         // Since we're cropping to a square, take the smallest size as the new dimension.
         int size = Math.min(image.height, image.width);
         FImage squareImg = image.extractCenter(size, size);
-
         // Resize to a fixed resolution
         FImage tinyImage = squareImg.process(new ResizeProcessor(this.tinySize, this.tinySize));
 
-        double[] test = tinyImage.getDoublePixelVector();
-
-
-        ArrayUtils.reshape(tinyImage.pixels);
-
-
-
-        System.out.println(Arrays.toString(test));
-        return null;
+        return tinyImage;
     }
+
+
 }
